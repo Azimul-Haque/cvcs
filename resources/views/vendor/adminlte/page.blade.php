ea7f208c1c40dd6835a6cc0a8e4fb0eadbf81baa
orbachinujbuk <orbachinujbuk@gmail.com>
2019-04-07 05:19:29 +0600

    Some Changes

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -102,7 +102,7 @@
                               <!-- Menu Footer-->
                               <li class="user-footer">
                                 <div class="pull-left">
-                                  <a href="{{ route('dashboard.profile') }}" class="btn btn-default btn-flat">প্রোফাইল</a>
+                                  <a href="{{ route('dashboard.profile') }}" class="btn btn-default btn-flat"><i class="fa fa-fw fa-user-o"></i> প্রোফাইল</a>
                                 </div>
                                 <div class="pull-right">
                                   @if(config('adminlte.logout_method') == 'GET' || !config('adminlte.logout_method') && version_compare(\Illuminate\Foundation\Application::VERSION, '5.3.0', '<'))
