ca6c2194a5a8ce17cd3136cac673d443a01ece8e
orbachinujbuk <orbachinujbuk@gmail.com>
2019-05-18 23:53:30 +0600

    updates May 18, 2018

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -41,23 +41,12 @@ class BlogController extends Controller {
                   ->withArchives($archives);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $categories = Category::all();
         return view('blogs.create')->withCategories($categories);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $this->validate($request,array(
@@ -89,12 +78,6 @@ class BlogController extends Controller {
         return redirect()->route('index.profile', Auth::user()->unique_key);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
@@ -118,35 +101,16 @@ class BlogController extends Controller {
                 ->withArchives($archives);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
