fbc6d1bb61e5b1e003133283e2bac88a4de59c48
orbachinujbuk <orbachinujbuk@gmail.com>
2019-06-24 06:15:56 +0600

    Update June 24, 2019

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2516,7 +2516,7 @@ class DashboardController extends Controller
         curl_setopt($ch, CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // this is important
         $smsresult = curl_exec($ch);
         
         echo $smsresult;
