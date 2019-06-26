686b3cf68dd48ccd6b9d4903dbe7cba2b5f3da59
orbachinujbuk <orbachinujbuk@gmail.com>
2019-06-26 14:41:08 +0600

    sms 2.6.1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2518,18 +2518,7 @@ class DashboardController extends Controller
             'messageid'=>"1"
         );
 
-        // balance check
-        $data= array(
-            'username'=>config('sms.gp_username'),
-            'password'=>config('sms.gp_password'),
-            'apicode'=>"3",
-            'msisdn'=>"0",
-            'countrycode'=>"0",
-            'cli'=>"0",
-            'messagetype'=>"0",
-            'message'=>"0",
-            'messageid'=>"0"
-        );
+        
         
         // initialize send status
         $ch = curl_init(); // Initialize cURL
