902eb4fffd71ddf0594c5bba490eabb4b3b3a391
orbachinujbuk <orbachinujbuk@gmail.com>
2019-08-27 05:50:04 +0600

    Little Change

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -126,7 +126,7 @@
                                     <a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}" class="text-large"><i class="icon-key i-plain"></i> লগআউট</a>
                                 </li>
                                 <li>
-                                    <a href="{{ url('tutorial/video') }}" class="text-large"><i class="icon-video i-plain"></i> আবেদনের টিউটোরিয়াল</a>
+                                    <a href="{{ url('tutorial/video') }}" class="text-large"><i class="icon-video i-plain"></i> ভিডিও টিউটোরিয়াল</a>
                                 </li>
                             </ul>
                         </li>
