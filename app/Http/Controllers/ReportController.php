fe3f2a6996d6c9d22dbffb1aa0315a51eff0f793
orbachinujbuk <orbachinujbuk@gmail.com>
2019-11-12 07:57:37 +0600

    ASD

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -66,114 +66,31 @@ class ReportController extends Controller
     			                         ->where('is_archieved', 0)
     			                         ->where('member_id', $member->member_id)
     			                         ->first();
+    			$approvedcashformontly = $approvedfordashboard->totalamount - 5000; // without the membership money;
 
     			if($member->joining_date == '' || $member->joining_date == null || strtotime('31-01-2019') > strtotime($member->joining_date))
     			{
-    			  $startyear = 2019;
-    			  $today = date("Y-m-d H:i:s");
-    			  $thismonth = '2019-1-';
-    			  $approvedcash = $approvedfordashboard->totalamount - 5000; // without the membership money;
-    			  $from = Carbon::createFromFormat('Y-m-d', '2019-1-1');
-    			  $to = Carbon::createFromFormat('Y-m-d', $thismonth . '1');
-    			  $diff_in_months = $to->diffInMonths($from);
-    			  dd($diff_in_months);
-    			} 
-    			// $totalyear = $startyear + ceil($approvedcash/(500 * 12)) - 1; // get total year
-    			//   if(date('Y') > $totalyear) {
-    			//       $totalyear = date('Y');
-    			//   }
-    			//   for($i=$startyear; $i <= $totalyear; $i++)
-    			//     for($j=1; $j <= 12; $j++) {{--  strtotime("11-12-10") --}}
-    			//       php
-    			//         $thismonth = '01-'.$j.'-'.$i;
-    			//       endphp
-    			//       <tr>
-    			//         <td>{{ date('F Y', strtotime($thismonth)) }}</td>
-    			//         <td>
-    			//           if($approvedcash/500 > 0)
-    			//             <span>পরিশোধিত</span>
-    			//           elseif(date('Y-m-d H:i:s', strtotime($thismonth)) < $today)
-    			//             <span style="color: red;">পরিশোধনীয়</span>
-    			//           endif
-    			//         </td>
-    			//         <td>
-    			//           if($approvedcash/500 > 0)
-    			//             ৳ 500
-    			//           endif
-    			//         </td>
-    			//       </tr>
-    			//       php
-    			//         $approvedcash = $approvedcash - 500;
-    			//       endphp
-    			//     endfor
-    			//   endfor
-    			// else
-    			//   php
-    			//       $startyear = date('Y', strtotime($member->joining_date));
-    			//       $startmonth = date('m', strtotime($member->joining_date));
-    			//       $today = date("Y-m-d H:i:s");
-    			//       $approvedcash = $approvedfordashboard->totalamount - 5000; // without the membership money;
-    			//       $endyear = $startyear + ceil($approvedcash/(500 * 12)) - 1; // get total year
-    			//       if(date('Y') > $endyear) {
-    			//           $endyear = date('Y');
-    			//       }
-    			//       $totalyears = $endyear - $startyear;
-    			//       $totalmonths = ($totalyears * 12) + (12 - $startmonth + 1);
-    			//   endphp
-    			//   php
-    			//     $thisyear = $startyear;
-    			//     $fractionyearsmonths = $totalmonths % 12;
-    			//     $fractionyearsmonthscount = $fractionyearsmonths;
-    			//     $monthsarray = [];
-    			//   endphp
-    			//   for($i=1; $i <= $fractionyearsmonthscount; $i++)
-    			//     php
-    			//       $monthsarray[] = '01-'.(12-$fractionyearsmonths + 1).'-'.$thisyear;
-
-    			//       $fractionyearsmonths--; // this ends with 0;
-    			//     endphp
-    			//   endfor
-
-    			//   php
-    			//     $leftmonths = $totalmonths - $fractionyearsmonthscount;
-    			//     if($leftmonths > 0) {
-    			//       $thisyear = $thisyear + 1;
-    			//       for ($j=1; $j <= $leftmonths; $j++) { 
-    			//         $thismonth = $j%12;
-    			//         if($thismonth == 0) {
-    			//           $thismonth = 12;
-    			//         }
-    			//         $monthsarray[] = '01-'.$thismonth.'-'.$thisyear;
-    			//         if($j%12 == 0) {
-    			//           $thisyear++;
-    			//         }
-    			//       }
-    			//     }
-    			    
-    			//   endphp
-    			//   foreach($monthsarray as $month)
-    			//     <tr>
-    			//       <td>{{ date('F Y', strtotime($month)) }}</td>
-    			//       <td>
-    			//         if($approvedcash/500 > 0)
-    			//           <span class="badge badge-success"><i class="fa fa-check"></i>পরিশোধিত</span>
-    			//         elseif(date('Y-m-d H:i:s', strtotime($month)) < $today)
-    			//           <span class="badge badge-danger"><i class="fa fa-exclamation-triangle"></i> পরিশোধনীয়</span>
-    			//         endif
-    			//       </td>
-    			//       <td>
-    			//         if($approvedcash/500 > 0)
-    			//           ৳ 500
-    			//         endif
-    			//       </td>
-    			//     </tr>
-    			//     php
-    			//       $approvedcash = $approvedcash - 500;
-    			//     endphp
-    			//   endforeach
-    			// endif
-    			  
+    			    $thismonth = Carbon::now()->format('Y-m-');
+    			    $from = Carbon::createFromFormat('Y-m-d', '2019-1-1');
+    			    $to = Carbon::createFromFormat('Y-m-d', $thismonth . '1');
+    			    $totalmonthsformember = $to->diffInMonths($from) + 1;
+    			    if(($totalmonthsformember * 500) > $approvedcashformontly) {
+    			    	$totalpendingmonthly = ($totalmonthsformember * 500) - $approvedcashformontly;
+    			    	$totalmontlydues = $totalmontlydues + $totalpendingmonthly;
+    			    }
+    			} else {
+					$startmonth = date('Y-m-', strtotime($member->joining_date));
+					$thismonth = Carbon::now()->format('Y-m-');
+    			    $from = Carbon::createFromFormat('Y-m-d', $startmonth . '1');
+    			    $to = Carbon::createFromFormat('Y-m-d', $thismonth . '1');
+    			    $totalmonthsformember = $to->diffInMonths($from) + 1;
+    			    if(($totalmonthsformember * 500) > $approvedcashformontly) {
+    			    	$totalpendingmonthly = ($totalmonthsformember * 500) - $approvedcashformontly;
+    			    	$totalmontlydues = $totalmontlydues + $totalpendingmonthly;
+    			    }
+    			}
     		}
+    		// dd($totalmontlydues); 
     		$pdf = PDF::loadView('dashboard.reports.pdf.allpaymentsandpendings', ['registeredmembers' => $registeredmembers, 'totalapproved' => $totalapproved, 'totalmontlydues' => $totalmontlydues]);
     		$fileName = 'CVCS_General_Report.pdf';
     		return $pdf->stream($fileName); // download
