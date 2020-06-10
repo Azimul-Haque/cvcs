5c41c1f70ed4307064fd6b254c6998180906499c
orbachinujbuk <orbachinujbuk@gmail.com>
2020-06-10 14:25:02 +0600

    asdasd324

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -585,7 +585,7 @@ class IndexController extends Controller
         Artisan::call('cache:clear');
         Artisan::call('view:clear');
         Artisan::call('key:generate');
-        Artisan::call('config:cache');
+        Artisan::call('config:clear');
         Session::flush();
         echo 'Config and Route Cached. All Cache Cleared';
     }
