e94624e4f716ddb2e5cbab20cd43472a7752de80
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-06-24 21:40:25 +0600

    added payment and transaction controller backend

--- app/Http/Controllers/FuadControllers/API/GeneralUserAppController.php
+++ app/Http/Controllers/FuadControllers/API/GeneralUserAppController.php
@@ -75,4 +75,66 @@ class GeneralUserAppController
             'approvedcountdashboard' => $approvedcountdashboard
         ], 200);
     }
+
+
+
+    public function getMemberTransactionSummary($unique_key){
+
+        $member = User::where('unique_key', $unique_key)->first();
+        if(!$member){
+            return response()->json("user with unique key does not exist", 404);
+        }
+
+        $membertotalpending = DB::table('payments')
+            ->select(DB::raw('SUM(amount) as totalamount'))
+            ->where('member_id', $member->member_id)
+            ->where('payment_status', 0)
+            ->where('is_archieved', 0)
+            // ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"), "=", Carbon::now()->format('Y-m'))
+            // ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"))
+            ->first();
+
+        $membertotalapproved = DB::table('payments')
+            ->select(DB::raw('SUM(amount) as totalamount'))
+            ->where('member_id', $member->member_id)
+            ->where('payment_status', '=', 1)
+            ->where('is_archieved', '=', 0)
+            // ->where(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"), "=", Carbon::now()->format('Y-m'))
+            // ->groupBy(DB::raw("DATE_FORMAT(created_at, '%Y-%m')"))
+            ->first();
+        $membertotalmontlypaid = DB::table('payments')
+            ->select(DB::raw('SUM(amount) as totalamount'))
+            ->where('payment_status', 1)
+            ->where('is_archieved', 0)
+            ->where('payment_category', 1) // 1 means monthly, 0 for membership
+            ->where('member_id', $member->member_id)
+            ->first();
+
+
+        return response()->json([
+            'membertotalpending' => $membertotalpending,
+            'membertotalapproved' => $membertotalapproved,
+            'membertotalmontlypaid' => $membertotalmontlypaid,
+        ], 200);
+    }
+
+
+    public function getMemberPayments($unique_key){
+        $member = User::where('unique_key', $unique_key)->first();
+        if(!$member){
+            return response()->json("user with unique key does not exist", 404);
+        }
+
+        $payments = Payment::where('member_id', $member->member_id)
+            ->where('is_archieved', 0)
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+
+        foreach ($payments as $payment){
+            $payment->payee = $payment->payee->name_bangla;
+        }
+
+        return response()->json($payments, 200);
+    }
+
 }
