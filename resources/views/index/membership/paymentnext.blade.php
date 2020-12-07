37a865bbd44a3276af9c796627dd634545486508
orbachinujbuk <orbachinujbuk@gmail.com>
2020-12-08 01:32:14 +0600

    SD324

--- resources/views/index/membership/paymentnext.blade.php
+++ resources/views/index/membership/paymentnext.blade.php
@@ -16,13 +16,14 @@
             অনলাইনে পরিশোধ করুন
           </h2>
           <center>
+            @if($member->payment_status == 'Unpaid')
               <h3 class="margin-two">অনুগ্রহ করে <b><u>৳{{ $member->application_payment_amount }}</u></b> পেমেন্ট গেটওয়ের মাধ্যমে পরিশোধ করুন</h3>
               <h4>
                 CVCS Trx_ID: <span style="color: #008D4C;"><b>{{ $member->trxid }}</b></span>
                 {{-- <span class="blinking_trx_id_text">(পেমেন্টজনিত জটিলতা এড়াতে নম্বরটি সংরক্ষণ করুন)</span> --}}
               </h4>
-              {{-- <h4>কারিগরি সমস্যার কারণে সাময়িক সময়ের জন্য বিকাশ পেমেন্ট করতে গ্রাহককে নিরুৎসাহিত করা হচ্ছে</h4> --}}
-              <div style="border: 2px solid #ddd; padding: 0px; width: 100%; padding: 10px;">
+              {{-- <h4>কারিগরি সমস্যার কারণে সাময়িক সময়ের জন্য বিকাশ পেমেন্ট করতে গ্রাহককে নিরুৎসাহিত করা হচ্ছে</h4> --}}              
+              <div style="border: 2px solid #ddd; padding: 0px; width: 100%; padding: 10px;" >
                   <img src="{{ asset('images/aamarpay.png') }}" class="img-responsive margin-two">
                   {!! 
                   aamarpay_post_button([
@@ -40,6 +41,9 @@
                       'opt_b' => $member->application_payment_amount
                   ], $member->application_payment_amount, '<i class="fa fa-money"></i> Pay Through AamarPay', 'btn btn-success') !!}
               </div>
+            @else
+              <h4>আপনার পেমেন্ট সম্পন্ন হয়েছে!</h4>
+            @endif
           </center>
         </div>
       </div>
