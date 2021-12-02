4cfc7371dcd4870e4cf3da181780b3ad58b7814c
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2021-12-03 03:40:48 +0600

    ASDASDASd

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -289,6 +289,6 @@ class ReportController extends Controller
 
         $pdf = PDF::loadView('dashboard.reports.pdf.daterangepayment', ['startdate' => $request->startdate, 'enddate' => $request->enddate, 'payments' => $payments]);
         $fileName = 'CVCS_'. date('Y-m-d', strtotime($request->startdate)) .'-to-'. date('Y-m-d', strtotime($request->enddate)) .'_Report.pdf';
-        return $pdf->download($fileName); // download
+        return $pdf->stream($fileName); // download
     }
 }
