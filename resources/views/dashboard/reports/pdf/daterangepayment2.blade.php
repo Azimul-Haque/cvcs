29a0e8a7d66ca5629d6ecfb1038b1855e2789aa5
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2022-01-01 03:54:09 +0600

    ASD3

--- resources/views/dashboard/reports/pdf/daterangepayment2.blade.php
+++ resources/views/dashboard/reports/pdf/daterangepayment2.blade.php
@@ -89,7 +89,7 @@
                     // $from = Carbon::createFromFormat('Y-m-d', strtotime('2019-1-1'));
                     $from = Carbon\Carbon::parse('2019-1-1');
                     // $to = Carbon::createFromFormat('Y-m-d', strtotime($enddate));
-                    $to = Carbon\Carbon::parse($enddate);
+                    $to = Carbon\Carbon::parse($enddate . '11:59:59');
                     echo $from . '<br/>';
                     echo $to . '<br/>';
                     $totalmonthsformember = $to->diffInDays($from);
