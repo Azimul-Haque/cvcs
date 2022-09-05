9c0e59b6da7eed0f31c5756561af2ebca14138a7
Azimul-Haque <orbachinujbuk@gmail.com>
2022-09-06 02:11:30 +0600

    Commit Tue 09/06/2022  2:11:30.31 17528

--- resources/views/dashboard/reports/pdf/designationmemberslist.blade.php
+++ resources/views/dashboard/reports/pdf/designationmemberslist.blade.php
@@ -38,38 +38,63 @@
     কাস্টমস এন্ড ভ্যাট কো-অপারেটিভ সোসাইটি
   </h2>
   <p align="center" style="padding-top: -20px;">
-    <span style="font-size: 20px;">সদস্য তালিকা ({{ $position->name }})</span><br/>
+    <span style="font-size: 20px;">সদস্যভিত্তিক পরিশোধ-বকেয়া রিপোর্ট ({{ $branch->name }})</span><br/>
   </p>
   
   <div class="" style="padding-top: 0px;">
     <table class="">
       <tr class="graybackground">
-        <th width="7%">#</th>
-        <th width="30%">সদস্য</th>
-        <th width="13%">মেম্বার আইডি</th>
-        <th>যোগাযোগ</th>
-        <th width="19%">অফিস তথ্য</th>
+        <th width="5%">#</th>
+        <th width="40%">সদস্য</th>
         <th>ছবি</th>
+        <th>সদস্যপদ বাবদ পরিশোধ</th>
+        <th>হিসাব শুরুর মাস</th>
+        <th>মোট মাসিক কিস্তি পরিশোধ<br/>({{ bangla(date('F, Y')) }} পর্যন্ত)</th>
+        <th>মোট মাসিক কিস্তি বকেয়া<br/>({{ bangla(date('F, Y')) }} পর্যন্ত)</th>
       </tr>
       @php
         $counter = 1;
       @endphp
-      @foreach($members as $member)
+      @foreach($members->sortByDesc('totalpendingmonthly') as $member)
         <tr>
-          <td align="center">{{ bangla($counter++) }}</td>
-          <td>{{ $member->name_bangla }}<br/><span style="font-family: Calibri; font-size: 12px;">{{ $member->name }}</span></td>
-          <td align="center"><span style="font-family: Calibri;"><b>{{ $member->member_id }}</b></span></td>
-          <td style="font-family: Calibri;">{{ $member->mobile }}<br/><small>{{ $member->email }}</small></td>
-          <td align="center"><small><span style="color: #074EA2;">{{ $member->position->name }}</span><br/>{{ $member->branch->name }}</small></td>
-          <td align="center">
+          <td align="center">{{ bangla($counter) }}</td>
+          <td>
+            {{ $member->name_bangla }}, <small>{{ $member->position->name }}</small><br/>
+            <small>আইডিঃ {{ $member->member_id }}, ফোনঃ {{ $member->mobile }}</small>
+          </td>
+          <td>
             @if($member->image != null && file_exists(public_path('images/users/'.$member->image)))
               <img src="{{ public_path('images/users/'.$member->image)}}" style="height: 50px; width: auto;" />
             @else
               <img src="{{ public_path('images/user.png')}}" style="height: 50px; width: auto;" />
             @endif
           </td>
+          <td align="center">৳ ২০০০</td>
+          <td align="center">
+            @if($member->joining_date == '' || $member->joining_date == null || strtotime('31-01-2019') > strtotime($member->joining_date))
+              {{ bangla(date('F, Y', strtotime('31-01-2019'))) }}
+            @else
+              {{ bangla(date('F, Y', strtotime($member->joining_date))) }}
+            @endif
+          </td>
+          <td align="center">৳ {{ bangla($member->payments->sum('amount')) }}</td>
+          <td align="center">৳ {{ bangla($member->totalpendingmonthly) }}</td>
         </tr>
-      @endforeach      
+        @php
+          $counter++;
+        @endphp
+      @endforeach
+
+      <tr class="graybackground">
+        <th width="5%"></th>
+        <th></th>
+        <th align="right">মোট</th>
+        <th>৳ {{ bangla($members->count() * 2000) }}</th>
+        <th></th>
+        <th>৳ {{ bangla($intotalmontlypaid) }}</th>
+        <th>৳ {{ bangla($intotalmontlydues) }}</th>
+      </tr>
+      
     </table>
   </div>
  
