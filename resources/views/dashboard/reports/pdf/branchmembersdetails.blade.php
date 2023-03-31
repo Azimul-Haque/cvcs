b74396c86257e75b202f9e290f378b3d4083b62e
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-31 23:32:20 +0600

    Commit Fri 03/31/2023 23:32:20.66 458

--- resources/views/dashboard/reports/pdf/branchmembersdetails.blade.php
+++ resources/views/dashboard/reports/pdf/branchmembersdetails.blade.php
@@ -67,7 +67,7 @@
         }
         $adhocmemberscol1 = collect($adhocmembers1);
         $adhocmemberscol2 = collect($adhocmembers2);
-        // dd($adhocmembers1);
+        dd($adhocmembers2);
         $adhocmemberscol1->merge($adhocmemberscol2);
       @endphp
       @foreach($adhocmemberscol1 as $member) 
