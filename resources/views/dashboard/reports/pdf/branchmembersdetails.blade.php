bd1a72c2c98cf0a0e84a634986a8a4b2e30e3f77
Azimul-Haque <orbachinujbuk@gmail.com>
2023-03-31 20:42:32 +0600

    Commit Fri 03/31/2023 20:42:32.29 7088

--- resources/views/dashboard/reports/pdf/branchmembersdetails.blade.php
+++ resources/views/dashboard/reports/pdf/branchmembersdetails.blade.php
@@ -55,7 +55,7 @@
       @php
         $counter = 1;
       @endphp
-      @foreach($members->sortByAsc('position_id') as $member)
+      @foreach($members->sortBy('position_id') as $member)
         <tr>
           <td align="center">{{ bangla($counter) }}</td>
           <td>
