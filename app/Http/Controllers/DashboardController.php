13d95f16ea2340d0c5ee2bf75f0f6ee76ce66ab1
Azimul-Haque <orbachinujbuk@gmail.com>
2023-04-04 01:25:57 +0600

    Commit Tue 04/04/2023  1:25:57.27 24810

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1955,7 +1955,12 @@ class DashboardController extends Controller
             } else {
                 $output .= '<td><img src="'. asset('images/user.png') .'" style="height: 50px; width: auto;" /></td>';
             }
-            $officelist = 
+            foreach($branches as $branch) {
+                $officelist = 
+              <option value="' . $row->branch_id . '">' . $row->branch->name . '</option>
+            }
+            
+            
             $output .= '<td><a class="btn btn-sm btn-success" href="'. route('dashboard.singlemember', $row->unique_key) .'" title="সদস্য তথ্য দেখুন"><i class="fa fa-eye"></i></a>
                 <a class="btn btn-sm btn-primary" href="'. route('dashboard.singleapplicationedit', $row->unique_key) .'" title="আবেদনটি সম্পাদনা করুণ"><i class="fa fa-edit"></i></a>
                 <a class="btn btn-sm btn-warning" data-toggle="modal" data-target="#sendToDefectiveListModal'. $row->member_id .'" data-backdrop="static" title="সদস্যের দপ্তর পরিবর্তন করুন"><i class="fa fa-fw fa-exchange" aria-hidden="true"></i></a>
