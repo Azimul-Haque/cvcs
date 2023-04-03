bfeb31b39ebcfce53c67a4b368599b8e4716c055
Azimul-Haque <orbachinujbuk@gmail.com>
2023-04-04 00:45:39 +0600

    Commit Tue 04/04/2023  0:45:39.27 10199

--- resources/views/dashboard/membership/members.blade.php
+++ resources/views/dashboard/membership/members.blade.php
@@ -84,7 +84,7 @@
               <a class="btn btn-sm btn-success" href="{{ route('dashboard.singlemember', $member->unique_key) }}" title="সদস্য তথ্য দেখুন"><i class="fa fa-eye"></i></a>
               {{-- <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#deleteMemberModal{{ $member->id }}" data-backdrop="static"><i class="fa fa-trash-o"></i></button> --}}
               <a class="btn btn-sm btn-primary" href="{{ route('dashboard.singleapplicationedit', $member->unique_key) }}" title="সদস্য তথ্য সম্পাদনা করুণ"><i class="fa fa-edit"></i></a>
-              <a class="btn btn-sm btn-warning" href="{{ route('dashboard.singleapplicationedit', $member->unique_key) }}" title="সদস্য তথ্য সম্পাদনা করুণ"><i class="fa fa-exchange"></i></a>
+              <a class="btn btn-sm btn-warning" title="সদস্য তথ্য সম্পাদনা করুণ"><i class="fa fa-exchange"></i></a>
             </td>
           </tr>
           @endforeach
