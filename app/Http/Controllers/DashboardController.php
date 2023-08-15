edac88c0242ce9fcecf71758b39148a9e9662979
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-16 01:10:56 +0600

    Commit Wed 08/16/2023  1:10:56.23 2081

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1867,7 +1867,22 @@ class DashboardController extends Controller
                        ->orderBy('position_id', 'asc')
                        // ->paginate(20);
                        ->get();
-
+        $counter = 1;
+        $adhocmembers1 = [];
+        $adhocmemberscol1 = collect();
+        $adhocmembers2 = [];
+        $adhocmemberscol2 = collect();
+        foreach($members as $member) {
+          if($member->position_id == 34) {
+            $adhocmembers1[] = $member;
+          } else {
+            $adhocmembers2[] = $member;
+          }
+        }
+        $adhocmemberscol1 = collect($adhocmembers1);
+        $adhocmemberscol2 = collect($adhocmembers2);
+        $mergedmembers = collect();
+        $mergedmembers = $adhocmemberscol1->merge($adhocmemberscol2);
         $ordered_member_array = [];
         foreach ($members as $member) {
             $ordered_member_array[(int) substr($member->member_id, -5)] = $member;
