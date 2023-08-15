9248a86207c3ad465286189f75491fb9a89a6d5c
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-16 01:27:28 +0600

    Commit Wed 08/16/2023  1:27:28.27 18266

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2118,22 +2118,22 @@ class DashboardController extends Controller
             }
 
             // SORT IT WITH সদস্য DESIGNEATION              
-                    // SORT IT WITH সদস্য DESIGNEATION              
-                    $adhocmembers1 = [];
-                    $adhocmemberscol1 = collect();
-                    $adhocmembers2 = [];
-                    $adhocmemberscol2 = collect();
-                    foreach($members as $member) {
-                      if($member->position_id == 34) {
-                        $adhocmembers1[] = $member;
-                      } else {
-                        $adhocmembers2[] = $member;
-                      }
-                    }
-                    $adhocmemberscol1 = collect($adhocmembers1);
-                    $adhocmemberscol2 = collect($adhocmembers2);
-                    $mergedmembers = collect();
-                    $mergedmembers = $adhocmemberscol1->merge($adhocmemberscol2);
+            // SORT IT WITH সদস্য DESIGNEATION              
+            $adhocmembers1 = [];
+            $adhocmemberscol1 = collect();
+            $adhocmembers2 = [];
+            $adhocmemberscol2 = collect();
+            foreach($members as $member) {
+              if($member->position_id == 34) {
+                $adhocmembers1[] = $member;
+              } else {
+                $adhocmembers2[] = $member;
+              }
+            }
+            $adhocmemberscol1 = collect($adhocmembers1);
+            $adhocmemberscol2 = collect($adhocmembers2);
+            $mergedmembers = collect();
+            $mergedmembers = $adhocmemberscol1->merge($adhocmemberscol2);
           }
 
           $total_row = count($members);
