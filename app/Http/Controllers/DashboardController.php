73ef6ee676c2424c16d007c580f8b0295a6c9734
Azimul-Haque <orbachinujbuk@gmail.com>
2023-08-16 01:15:46 +0600

    Commit Wed 08/16/2023  1:15:46.26 28947

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1864,6 +1864,12 @@ class DashboardController extends Controller
         $memberscount = User::where('activation_status', 1)->where('role_type', '!=', 'admin')->count();
         $members = User::where('activation_status', 1)
                        ->where('role_type', '!=', 'admin')
+                       ->with(['payments' => function ($query) {
+                            // $query->orderBy('created_at', 'desc');
+                            $query->where('payment_status', '=', 1);
+                            $query->where('is_archieved', '=', 0);
+                            $query->where('payment_category', 1);  // 1 means monthly, 0 for membership
+                        }])  
                        ->orderBy('position_id', 'asc')
                        // ->paginate(20);
                        ->get();
