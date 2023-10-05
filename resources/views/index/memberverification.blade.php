209908bf8a1b020644f13ed85ba72c027a449a83
Azimul-Haque <orbachinujbuk@gmail.com>
2023-10-06 03:21:36 +0600

    Commit Fri 10/06/2023  3:21:36.34 10413

--- resources/views/index/memberverification.blade.php
+++ resources/views/index/memberverification.blade.php
@@ -26,7 +26,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6 col-md-offset-3">
-                    @if($member->count() > 0)
+                    @if($member)
                         পাওয়া গেসে
                     @else
                         পাওয়া যায়নি!
