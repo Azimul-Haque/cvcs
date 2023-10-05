85ee75fb4789cbd9ea49a385f85c147e5d07083e
Azimul-Haque <orbachinujbuk@gmail.com>
2023-10-06 03:57:36 +0600

    Commit Fri 10/06/2023  3:57:36.33 18511

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -786,7 +786,7 @@ class IndexController extends Controller
     // clear configs, routes and serve
     public function clear()
     {
-        Artisan::call('route:clear');
+        // Artisan::call('route:clear');
         // Artisan::call('optimize');
         // Artisan::call('cache:clear');
         // Artisan::call('view:clear');
