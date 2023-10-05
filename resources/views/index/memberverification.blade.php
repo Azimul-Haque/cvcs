97869f979233586da480840905aed2e1cb793112
Azimul-Haque <orbachinujbuk@gmail.com>
2023-10-06 03:19:46 +0600

    Commit Fri 10/06/2023  3:19:46.24 26684

---
+++ resources/views/index/memberverification.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.index')
+@section('title')
+    CVCS | Events
+@endsection
+
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/stylesheet.css') }}">
+@endsection
+
+@section('content')
+    <!-- head section -->
+    <section class="content-top-margin page-title-small bg-gray">
+        <div class="container">
+            <div class="row">
+                <!-- section title -->
+                <div class="col-md-12 col-sm-12 xs-margin-bottom-four wow fadeInUp">
+                    <span class="text-large letter-spacing-2 black-text font-weight-600 text-uppercase agency-title">ইভেন্ট তালিকা</span>
+                </div>
+                <!-- end section title -->
+            </div>
+        </div>
+    </section>
+    <!-- end head section -->
+
+    <section style="padding: 13px 0;">
+        <div class="container">
+            <div class="row">
+                @foreach($events as $event)
+                    <div class="col-md-4 col-sm-6 sm-margin-bottom-ten xs-text-center" style="min-height: 370px;">
+                        <div class="blog-post">
+                            <div class="blog-post-images">
+                                <a href="{{ route('index.singleevent', $event->id) }}">
+                                    @if(($event->image != null) || !file_exists(public_path('images/events/'.$event->image)))
+                                        <img src="{{ asset('images/events/'.$event->image) }}" alt="">
+                                    @else
+                                        <img src="{{ asset('images/events/default_event.jpg') }}" alt="">
+                                    @endif
+                                </a>
+                            </div>
+                            <div class="post-details">
+                                <a href="{{ route('index.singleevent', $event->id) }}" class="post-title text-large">{{ $event->name }}</a>
+                                <span class="post-author">
+                                    {{ date('F d, Y', strtotime($event->created_at)) }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('js')
+   
+@endsection
