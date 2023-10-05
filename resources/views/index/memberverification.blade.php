2e0cda7533226292f87166913a2df2f3054b9edd
Azimul-Haque <orbachinujbuk@gmail.com>
2023-10-06 04:18:02 +0600

    Commit Fri 10/06/2023  4:18:02.25 6429

--- resources/views/index/memberverification.blade.php
+++ resources/views/index/memberverification.blade.php
@@ -59,7 +59,7 @@
                                 </tr>
                             </tbody>
                         </table>
-                        {{-- <img src="{!! QrCode::format('png')->size(200)->generate('Make me into a QrCode!', 'image/png'); !!}"> --}}
+                        <img src="{!! QrCode::format('png')->size(200)->generate('Make me into a QrCode!', 'image/png'); !!}">
                         {{ QrCode::size(200)->generate('Make me into a QrCode!') }}
                     @else
                         <center>
