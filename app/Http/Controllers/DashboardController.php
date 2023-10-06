87b12f8ea0c1024c1677a3e70cada11220f9326c
Azimul-Haque <orbachinujbuk@gmail.com>
2023-10-06 21:43:10 +0600

    Commit Fri 10/06/2023 21:43:10.26 733

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4136,7 +4136,7 @@ class DashboardController extends Controller
                      ->where('activation_status', 1)
                      ->orderBy('id', 'asc')
                      ->orderBy('position_id', 'asc')
-                     ->get()->take(1000);
+                     ->get()->take(100);
 
         return view('dashboard.excelexport')->withUsers($users);
     }
