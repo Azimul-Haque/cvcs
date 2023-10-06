d2ac2823b5fca98d7db707e9da242c0198fdebdd
Azimul-Haque <orbachinujbuk@gmail.com>
2023-10-06 21:25:44 +0600

    Commit Fri 10/06/2023 21:25:44.32 12647

--- resources/views/dashboard/excelexport.blade.php
+++ resources/views/dashboard/excelexport.blade.php
@@ -47,9 +47,9 @@
 					<td>{{ date('F d, Y', strtotime($user->joining_date)) }}</td>
 					<td>{{ $user->position ? $user->position->name : '' }}</td>
 					<td>
-						{!! QrCode::size(150)->generate(route('index.memberverification', $user->member_id)); !!}
+						{{-- {!! QrCode::size(150)->generate(route('index.memberverification', $user->member_id)); !!} --}}
 						{{-- <img src="data:image/png;base64,{{ base64_encode(SimpleSoftwareIO\QrCode\Facades\QrCode::size(300)->format('png')->generate('Make me into a QrCode!') ) }}"> --}}
-						<img src="data:image/png;base64,{{ base64_encode(QrCode::size(150)->format('png')->generate('Make me into a QrCode!') ) }}">
+						<img src="data:image/png;base64,{{ base64_encode(QrCode::size(150)->format('png')->generate(route('index.memberverification', $user->member_id)) ) }}">
 					</td>
 					{{-- <td>{!! QrCode::size(200)->format('eps')->generate('ItSolutionStuff.com', public_path('images/qrcode.eps')); !!}</td> --}}
 				</tr>
