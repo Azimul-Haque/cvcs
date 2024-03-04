1ae040bb9a7b9daf1519caf55a705a9b484d2b0e
Azimul-Haque <orbachinujbuk@gmail.com>
2024-03-04 23:39:44 +0600

    Commit Mon 03/04/2024 23:39:44.30 11936

--- resources/views/dashboard/excelexport.blade.php
+++ resources/views/dashboard/excelexport.blade.php
@@ -51,7 +51,7 @@
 					<td>
 						{{-- {!! QrCode::size(150)->generate(route('index.memberverification', $user->member_id)); !!} --}}
 						{{-- <img src="data:image/png;base64,{{ base64_encode(SimpleSoftwareIO\QrCode\Facades\QrCode::size(300)->format('png')->generate('Make me into a QrCode!') ) }}"> --}}
-						<img src="data:image/png;base64,{{ base64_encode(QrCode::size(150)->format('png')->generate(route('index.memberverification', $user->member_id)) ) }}">
+						{{-- <img src="data:image/png;base64,{{ base64_encode(QrCode::size(150)->format('png')->generate(route('index.memberverification', $user->member_id)) ) }}"> --}}
 					</td>
 					{{-- <td>{!! QrCode::size(200)->format('eps')->generate('ItSolutionStuff.com', public_path('images/qrcode.eps')); !!}</td> --}}
 				</tr>
