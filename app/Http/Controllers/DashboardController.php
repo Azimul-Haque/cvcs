2cce3832ca885afd325e3ba955895dbf998ce462
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-07 00:07:18 +0600

    Commit Tue 05/07/2024  0:07:18.23 6038

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1732,48 +1732,81 @@ class DashboardController extends Controller
         // $url = config('sms.url');
         // $number = $mobile_number;
         $text = $request->message . ' Customs and VAT Co-operative Society (CVCS).';
-        $data= array(
-            'username'=>config('sms.username'),
-            'password'=>config('sms.password'),
-            'number'=>"$number",
-            'message'=>"$text",
-            // 'apicode'=>"1",
-            // 'msisdn'=>"$number",
-            // 'countrycode'=>"880",
-            // 'cli'=>"CVCS",
-            // 'messagetype'=>"3",
-            // 'messageid'=>"3"
-        );
-        // initialize send status
-        $ch = curl_init(); // Initialize cURL
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // this is important
-        $smsresult = curl_exec($ch);
-        $p = explode("|",$smsresult);
-        $sendstatus = $p[0];
+        
+        // NEW PANEL
+        $url = config('sms.url2');
+        $api_key = config('sms.api_key');
+        $senderid = config('sms.senderid');
+        $number = $mobile_number;
+        $message = $text;
 
-        // $sendstatus = substr($smsresult, 0, 3);
-        // API Response Code
-        // 1000 = Invalid user or Password
-        // 1002 = Empty Number
-        // 1003 = Invalid message or empty message
-        // 1004 = Invalid number
-        // 1005 = All Number is Invalid 
-        // 1006 = insufficient Balance 
-        // 1009 = Inactive Account
-        // 1010 = Max number limit exceeded
-        // 1101 = Success
-        // send sms
-        if($sendstatus == 1101) {
+        $data = [
+            "api_key" => $api_key,
+            "senderid" => $senderid,
+            "number" => $number,
+            "message" => $message,
+        ];
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $jsonresponse = json_decode($response);
+
+        if($jsonresponse->response_code == 202) {
             Session::flash('success', 'SMS সফলভাবে পাঠানো হয়েছে!');
-        } elseif($sendstatus == 1006) {
+        } elseif($jsonresponse->response_code == 1007) {
             Session::flash('warning', 'অপর্যাপ্ত SMS ব্যালেন্সের কারণে SMS পাঠানো যায়নি!');
         } else {
             Session::flash('warning', 'দুঃখিত! SMS পাঠানো যায়নি!');
-            // return json_encode($smsresult);
         }
+        // NEW PANEL
+        
+        // $data= array(
+        //     'username'=>config('sms.username'),
+        //     'password'=>config('sms.password'),
+        //     'number'=>"$number",
+        //     'message'=>"$text",
+        //     // 'apicode'=>"1",
+        //     // 'msisdn'=>"$number",
+        //     // 'countrycode'=>"880",
+        //     // 'cli'=>"CVCS",
+        //     // 'messagetype'=>"3",
+        //     // 'messageid'=>"3"
+        // );
+        // // initialize send status
+        // $ch = curl_init(); // Initialize cURL
+        // curl_setopt($ch, CURLOPT_URL,$url);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // this is important
+        // $smsresult = curl_exec($ch);
+        // $p = explode("|",$smsresult);
+        // $sendstatus = $p[0];
+
+        // // $sendstatus = substr($smsresult, 0, 3);
+        // // API Response Code
+        // // 1000 = Invalid user or Password
+        // // 1002 = Empty Number
+        // // 1003 = Invalid message or empty message
+        // // 1004 = Invalid number
+        // // 1005 = All Number is Invalid 
+        // // 1006 = insufficient Balance 
+        // // 1009 = Inactive Account
+        // // 1010 = Max number limit exceeded
+        // // 1101 = Success
+        // // send sms
+        // if($sendstatus == 1101) {
+        //     Session::flash('success', 'SMS সফলভাবে পাঠানো হয়েছে!');
+        // } elseif($sendstatus == 1006) {
+        //     Session::flash('warning', 'অপর্যাপ্ত SMS ব্যালেন্সের কারণে SMS পাঠানো যায়নি!');
+        // } else {
+        //     Session::flash('warning', 'দুঃখিত! SMS পাঠানো যায়নি!');
+        //     // return json_encode($smsresult);
+        // }
 
         return redirect()->back();
     }
