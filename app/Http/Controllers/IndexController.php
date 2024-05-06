85969ad3e68a60961989ab1dd81da4c14460a2bc
Azimul-Haque <orbachinujbuk@gmail.com>
2024-05-07 00:24:17 +0600

    Commit Tue 05/07/2024  0:24:17.29 11099

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -521,8 +521,8 @@ class IndexController extends Controller
                     $mobile_number = substr($member->mobile, -11);
                 }
             }
-            // $url = config('sms.url');
-            // $number = $mobile_number;
+            $url = config('sms.url');
+            $number = $mobile_number;
             $text = 'Dear ' . $member->name . ', payment of tk. '. $amount_paid .' is received successfully, TrxID: ' . $member->trxid . '. Thanks. Customs and VAT Co-operative Society (CVCS). Login: https://cvcsbd.com/login';
             
             // NEW PANEL
@@ -634,8 +634,8 @@ class IndexController extends Controller
                   $mobile_number = substr($member->mobile, -11);
               }
           }
-          $url = config('sms.url');
-          $number = $mobile_number;
+          // $url = config('sms.url');
+          // $number = $mobile_number;
           $text = 'Dear ' . $member->name . ', payment of tk. '. $amount_paid .' is received successfully, TrxID: ' . $member->trxid . '. Thanks. Customs and VAT Co-operative Society (CVCS). Login: https://cvcsbd.com/login';
           
           // NEW PANEL
