e0bc2c061bb84c481b238e9c7889a296414b01cf
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-02-06 19:58:48 +0600

    Commit Fri 02/06/2026 19:58:48.21 13540

--- resources/views/dashboard/adminsandothers/branches.blade.php
+++ resources/views/dashboard/adminsandothers/branches.blade.php
@@ -66,25 +66,24 @@
                     <h4 class="modal-title">দপ্তর সম্পাদনা</h4>
                   </div>
                   {!! Form::model($branch, ['route' => ['dashboard.updatebranch', $branch->id], 'method' => 'PUT', 'class' => 'form-default']) !!}
-                        
-                  <div class="modal-body">
-                    <div class="form-group">
-                      {!! Form::label('name', 'কাস্টমস অফিসের নাম') !!}
-                      {!! Form::text('name', null, array('class' => 'form-control', 'id' => 'name', 'placeholder' => 'কাস্টমস অফিসের নাম', 'required' => '')) !!}
+                    <div class="modal-body">
+                      <div class="form-group">
+                        {!! Form::label('name', 'কাস্টমস অফিসের নাম') !!}
+                        {!! Form::text('name', null, array('class' => 'form-control', 'id' => 'name', 'placeholder' => 'কাস্টমস অফিসের নাম', 'required' => '')) !!}
+                      </div>
+                      <div class="form-group">
+                        {!! Form::label('address', 'কাস্টমস অফিসের ঠিকানা') !!}
+                        {!! Form::text('address', null, array('class' => 'form-control', 'id' => 'address', 'placeholder' => 'কাস্টমস অফিসের ঠিকানা', 'required' => '')) !!}
+                      </div>
+                      <div class="form-group">
+                        {!! Form::label('phone', 'কাস্টমস অফিসের ফোন') !!}
+                        {!! Form::number('phone', null, array('class' => 'form-control', 'id' => 'phone', 'placeholder' => 'কাস্টমস অফিসের ফোন', 'required' => '')) !!}
+                      </div>
                     </div>
-                    <div class="form-group">
-                      {!! Form::label('address', 'কাস্টমস অফিসের ঠিকানা') !!}
-                      {!! Form::text('address', null, array('class' => 'form-control', 'id' => 'address', 'placeholder' => 'কাস্টমস অফিসের ঠিকানা', 'required' => '')) !!}
+                    <div class="modal-footer">
+                      {!! Form::submit('দাখিল করুন', array('class' => 'btn btn-primary')) !!}
+                      <button type="button" class="btn btn-default" data-dismiss="modal">ফিরে যান</button>
                     </div>
-                    <div class="form-group">
-                      {!! Form::label('phone', 'কাস্টমস অফিসের ফোন') !!}
-                      {!! Form::number('phone', null, array('class' => 'form-control', 'id' => 'phone', 'placeholder' => 'কাস্টমস অফিসের ফোন', 'required' => '')) !!}
-                    </div>
-                  </div>
-                  <div class="modal-footer">
-                    {!! Form::submit('দাখিল করুন', array('class' => 'btn btn-primary')) !!}
-                    <button type="button" class="btn btn-default" data-dismiss="modal">ফিরে যান</button>
-                  </div>
                   {!! Form::close() !!}
                 </div>
               </div>
