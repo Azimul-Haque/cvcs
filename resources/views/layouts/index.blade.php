69fc0b927824b6101836de7d56d182147dca814b
Numaira-Home-PC <orbachinujbuk@gmail.com>
2026-05-05 22:02:47 +0600

    Commit Tue 05/05/2026 22:02:47.26 3160

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -157,7 +157,7 @@
                 <div class="row margin-three">
                     <!-- copyright -->
                     <div class="col-md-6 col-sm-6 col-xs-12 copyright text-left letter-spacing-1 xs-text-center xs-margin-bottom-one">
-                        &copy; {{ date('Y') }} CVCS. Developed by <a href="http://loencebd.com" class="text-light" target="_blank">InnovaTech Bangladesh</a>
+                        &copy; {{ date('Y') }} CVCS. Developed by <a href="http://loencebd.com" style="color: #ffffff;" target="_blank">InnovaTech Bangladesh</a>
                     </div>
                     <!-- end copyright -->
                     <!-- logo -->
